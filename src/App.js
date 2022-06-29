390e2b9fe508e70c3414aa63959443fae4c5ddec
Roman Yacenko <ryacenko3@gmail.com>
2022-06-29 14:41:37 +0300

    start clock!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './styles/style.scss';
+//sec - 6deg
+//minute - 6deg
+//hour - 30deg
 
-function App() {
+export const App = () => {
+  const [ time, setTime ] = useState(new Date().toLocaleTimeString());
+  
+  useEffect(() => {
+    const interval = 
+      window.setInterval(() => {
+        setTime(new Date().toLocaleTimeString());
+      }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [ time ])
+
+  const hours = time.split(':')[0];
+  const minute = time.split(':')[1];
+  const second = time.split(':')[2].slice(0,2);
+  const minuteDeg = minute * 6;
+  const secondDeg = second * 6;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="clock">
+        <div className="center" />
+        <div className="clock-stick-hour" style={{transform: `rotate(${hours > 12 ? (hours - 12) * 30 : hours * 30}deg)`}}/>
+        <div className="clock-stick-minutes" style={{transform: `rotate(${minuteDeg}deg)`}}/>
+        <div className="clock-stick-second" style={{transform: `rotate(${secondDeg}deg)`}}/>
+      </div>
     </div>
   );
 }
